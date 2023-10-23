6cb87871a42656c82f9061be5834a14e8dc92353
Bilol <acutmaxb@gmail.com>
2023-10-23 20:01:50 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/dsm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dsm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: Color(0xFFff87b7),
+        secondary: Color(0xFF4f4f4f),
+        background: Color(0xFFF4F4F4),
+      ),
+      fontFamily: 'Optima',
+      textTheme: TextTheme(
+        titleLarge: TextStyle(
+          color: Color(0xFF2B2E4A),
+          fontWeight: FontWeight.bold,
+          fontSize: 36,
+        ),
+        titleMedium: TextStyle(
+          color: Color(0xFF2B2E4A),
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
+      ));
+}
